38f2bbfc9ef0c7cd14335e3484a7c2da9cfaa64d
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-11-08 16:07:39 -0300

    JSX 1 - origamid

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+
+const Titulo = <h1>Esse é um titulo</h1>;
 
 function App() {
-  /**
-   * O useState é uma funçaõ que retorna um Array com 2 valores. O
-   * primeiro valor guarda o dado do estado atual, oide ser qualquer
-   * tipo de dado como string, array, números, boolean, null, undefined
-   * e objetos. O segundo valor é uma função que pode ser utilizada
-   * para, modificarmos o esdodo do primero valor.
-   *
-   * Quando a função de modificação do estado é ativada, todos os
-   * componentes que dependerem do estado, serão renderizados
-   * novamente e os seus filhos também. É isso que garante a
-   * reatividade de componentes funcionais no React.
-   */
-  const [ativo, setAtivo] = React.useState(true);
-  const [dados, setDados] = React.useState({ nome: 'André', idade: '30' });
+  const random = Math.random();
+  const ativo = false;
 
-  const [modal, setModal] = React.useState(false);
-
-  function handleClick(params) {
-    setAtivo(!ativo);
-    if (ativo === true) setDados({ ...dados, faculdade: 'Possui Faculdade' });
-    if (ativo === false)
-      setDados({ ...dados, faculdade: 'Não Possui Faculdade' });
+  function mostrarNome(sobrenome) {
+    return 'André' + sobrenome;
   }
-
+  const carro = {
+    marca: 'Ford',
+    rodas: '4',
+  };
+  const estilo = {
+    color: 'blue',
+  };
   return (
-    <div>
-      <p>Nome: {dados.nome}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>Faculdade: {dados.faculdade}</p>
-      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
-      <p></p>
-      <div>{modal ? 'Modal aberto' : 'Modal Fechado'}</div>
-      <ButtonModal modal={modal} setModal={setModal} />
-      <Modal modal={modal} setModal={setModal} />
-    </div>
+    <>
+      {Titulo}
+      <p>
+        {true ? '1' : '2'} - {10}
+        {mostrarNome('Rafael')}
+      </p>
+      <p style={{ estilo }}>{new Date().getFullYear()}</p>
+      <p>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{(random * 1000) / 50}</p>
+    </>
   );
 }
 
--- src/ButtonModal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonModal = ({ modal, setModal }) => {
-  if (modal === false)
-    return <button onClick={() => setModal(true)}> Abrir </button>;
-  return null;
-};
-
-export default ButtonModal;
