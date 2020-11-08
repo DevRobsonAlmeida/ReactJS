383c1c609ac54be6141101ba94f606ea6d1d94b4
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-11-08 16:28:30 -0300

    JSX Arrays 1 - origamid

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from 'react';
 
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: '2500' },
-    { nome: 'Geladeira', preco: '3000' },
-    { nome: 'Smarphone', preco: '1500' },
-    { nome: 'Guitarra', preco: '1500' },
-  ],
-  ativa: false,
-};
-const luana = {
-  cliente: 'Luana',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: '2500' },
-    { nome: 'Geladeira', preco: '3000' },
-    { nome: 'Smarphone', preco: '1500' },
-  ],
-  ativa: ativa,
-};
 function App() {
-  const dados = luana;
-
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace('R$ ', '')))
-    .reduce((a, b) => a + b);
+  const livros = [
+    { nome: 'A Game Of Thrones', ano: 1986 },
+    { nome: 'A Clash Of Kings', ano: 1998 },
+    { nome: 'A Storm Of Swords', ano: 2000 },
+  ];
 
   return (
     <>
-      <p>Nome:{dados.cliente}</p>
-      <p>Idade:{dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total: R$ {total}</p>
-      {total > 1000 && <p>Você está gastando muito</p>}
+      {livros
+        .filter(({ ano }) => ano >= 1988)
+        .map(({ nome, ano }) => (
+          <li key={nome}>
+            {' '}
+            {nome}, {ano}
+          </li>
+        ))}
     </>
   );
 }
