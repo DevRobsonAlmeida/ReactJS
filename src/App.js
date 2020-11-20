3ec3737cdb2f9b9918ccc230086bd3a3a0012aaa
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-11-20 11:01:16 -0300

    Event - Origamid

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
 import React from 'react';
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartgone',
-    preco: 'R$ 2000',
-    cor: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cor: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cor: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 function App() {
-  //Outra forma de filtrar
-  /** 
-   * const dados = produtos.filter(
-    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
-    );
-  */
+  function handleClick(event) {
+    console.log(event.target);
+  }
 
+  function handleScroll(event) {
+    console.log(event);
+  }
+  window.addEventListener('scroll', handleScroll);
   return (
     <>
-      <ul>
-        {produtos
-          .filter(({ preco }) => Number(preco.replace('R$ ', '')) > 1500)
-          .map(({ id, nome, preco, cor }) => (
-            <div key={id}>
-              <h1>{nome}</h1>
-              <p>Preco: {preco}</p>
-              <ul>
-                {cor.map((cor) => (
-                  <li
-                    style={{
-                      backgroundColor: cor,
-                      color: 'white',
-                      width: '200px',
-                    }}
-                    key={cor}
-                  >
-                    {cor}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-      </ul>
+      <button onClick={handleClick}>Clique</button>
+      {/*\ 
+        A baixo será feito um event no modo de função anônimo
+      \*/}
+      <button onClick={(event) => alert(event.target.innerText)}>
+        Compre violão
+      </button>
+      {/*\
+        Window/Document
+        Ao colocar um window ou document, devem ser adicionados JavaScript normalmente, usando o addEventListener.
+        handleScroll retornará uma resposta ao mexer no scroll, isso por causa do window acima
+      \*/}
+      <div style={{ height: '200vh' }}></div>
     </>
   );
 }
