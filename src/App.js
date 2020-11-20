d17f6d1ce11d31d2d210862b5ee0d5aeb05c095e
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-11-20 16:38:30 -0300

    UseStatus Parte 2/3

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
 
 const App = () => {
-  /**
-   * Explição basica
-   * O 'ativo' receberá primeiro valor.
-   * Já o 'setAtivo' tem a função de sempre atualizar o valor do 'ativo'
-   */
-  const [ativo, setAtivo] = React.useState(false);
-  const [dados, setDados] = React.useState({ nome: 'André', idade: '30' });
-  // Usando de modo Externo
-  const [modal, setModal] = React.useState(false);
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['item 1']);
 
-  // setAtivo(<atribuir o novo valor>)
   function handleClick() {
-    setAtivo(!ativo);
-    if (ativo === true)
-      setDados({ ...dados, faculdade: 'Possui Faculdade', idade: 50 });
-    else setDados({ ...dados, faculdade: 'Não Possui Faculdade', idade: 17 });
+    setContar(contar + 1);
+    setItems([...items, 'item ' + (contar + 1)]);
   }
-
   return (
     <>
-      {/* Modo interno */}
-      <p>{dados.nome}</p>
-      <p>{dados.idade}</p>
-      <p>{dados.faculdade}</p>
-      <button onClick={handleClick}>
-        {ativo ? 'Botão Ativo' : 'Botão Inativo'}
-      </button>
-
-      {/* Modo externo */}
-      <p></p>
-      <div>{modal ? 'Modal Aberto' : 'Modal Fechado'}</div>
-      <p>
-        <ButtonModal modal={modal} setModal={setModal} />
-        <Modal modal={modal} />
-      </p>
+      <button onClick={handleClick}>{contar}</button>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </>
   );
 };
--- src/Modal.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Modal = ({ modal }) => {
-  if (modal === true) return <div>Esse é um modal.</div>;
-  return null;
-};
-
-export default Modal;
