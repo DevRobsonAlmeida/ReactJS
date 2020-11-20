246f55f3c0c433e86fbc79ba105cb9a91ecec729
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-11-20 11:00:00 -0300

    Entendimento Basico de JSX Array 2 - desafio origamid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React from 'react';
 
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartgone',
+    preco: 'R$ 2000',
+    cor: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cor: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cor: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
 function App() {
-  const livros = [
-    { nome: 'A Game od Thrones', ano: 1996 },
-    { nome: 'A Clash of kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
+  //Outra forma de filtrar
+  /** 
+   * const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
+    );
+  */
+
   return (
     <>
       <ul>
-        {livros
-          .filter(({ ano }) => ano >= 1998)
-          .map(({ nome, ano }) => (
-            <li key={nome}>
-              {nome}, {ano}
-            </li>
+        {produtos
+          .filter(({ preco }) => Number(preco.replace('R$ ', '')) > 1500)
+          .map(({ id, nome, preco, cor }) => (
+            <div key={id}>
+              <h1>{nome}</h1>
+              <p>Preco: {preco}</p>
+              <ul>
+                {cor.map((cor) => (
+                  <li
+                    style={{
+                      backgroundColor: cor,
+                      color: 'white',
+                      width: '200px',
+                    }}
+                    key={cor}
+                  >
+                    {cor}
+                  </li>
+                ))}
+              </ul>
+            </div>
           ))}
       </ul>
     </>
