ab5f8ab549cf9a5a6c4282f574164d450c17e54f
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2020-12-30 13:53:26 -0300

    useContext 1/2

---
+++ src/Produto.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { GlobalContext } from './GlobalContext';
+
+const Produto = () => {
+  const global = React.useContext(GlobalContext);
+  console.log(global);
+
+  return (
+    <div>
+      Produto:
+      {global.contar}
+      <br />
+      <button onClick={() => global.adicionarDois()}>Adicionar</button>
+    </div>
+  );
+};
+
+export default Produto;
