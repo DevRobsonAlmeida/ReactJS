456f955c43e20c8b603f575bc7bb3f09021b3e74
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2021-01-04 14:56:21 -0300

    Custom Hook 2 - useFetch e useCallback

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import useLocalStorage from './useLocalStorage';
+import useFetch from './useFetch';
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage('Produto', '');
+  const [produto, setProduto] = useLocalStorage('Produto', 'null');
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fecthData() {
+      const { response, json } = await request(
+        'https://ranekapi.origamid.dev/json/api/produto/',
+      );
+      console.log(response, json);
+    }
+    fecthData();
+  }, [request]);
 
   function handleClick({ target: { innerText } }) {
     setProduto(innerText);
   }
 
-  return (
-    <div>
-      <p>Produto Preferido: {produto}</p>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick} style={{ margin: '.5rem' }}>
-        smartphone
-      </button>
-    </div>
-  );
+  if (error) return <p>{error}</p>;
+  if (loading) return <div>Carregando...</div>;
+  if (data)
+    return (
+      <div>
+        <p>Produto Preferido: {produto}</p>
+        <button onClick={handleClick}>notebook</button>
+        <button onClick={handleClick} style={{ margin: '.5rem' }}>
+          smartphone
+        </button>
+
+        {data.map((produto) => (
+          <div key={handleClick}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
 
 export default App;
