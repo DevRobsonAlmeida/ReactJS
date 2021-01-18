9d4a3abc60d92880ba1c355fd2e96d701fa5c2e6
DevRobsonAlmeida <robsonalmeida073@gmail.com>
2021-01-17 21:01:53 -0300

    Componentes Checkbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Checkbox from './Form/Checkbox';
 import Input from './Form/Input';
 import Radio from './Form/Radio';
 import Select from './Form/Select';
@@ -9,16 +10,32 @@ const App = () => {
   const [produto, setProduto] = React.useState('');
   const [cor, setCor] = React.useState('');
   const [frutas, setFrutas] = React.useState('');
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
 
   return (
     <form>
-      <h2>Retorno</h2>
+      <h1>Retorno</h1>
       <p>Nome: {nome}</p>
       <p>Email: {email}</p>
       <p>Frutas: {frutas}</p>
       <p>Cor: {cor} </p>
       <p>Produto: {produto}</p>
+      <p>linguagens: {linguagens + ''}</p>
+      <p>Termos: {termos}</p>
 
+      <h3>Termos</h3>
+      <Checkbox
+        options={['Li e aceito os termos.']}
+        value={termos}
+        setValue={setTermos}
+      />
+      <h3>Linguagens</h3>
+      <Checkbox
+        options={['Java', 'Php', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
       <h3>Cores</h3>
       <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <h3>Frutas</h3>
